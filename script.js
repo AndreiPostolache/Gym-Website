d283c2014c152b3cf0ad7286bc7bed53cb7c065a
Andrei-Catalin Postolache <andreic.postolache555@yahoo.com>
2023-05-14 19:50:43 +0300

    making it responsive

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
-// const allExercices = async function () {
-//   const url = "https://exercisedb.p.rapidapi.com/exercises";
-//   const options = {
-//     method: "GET",
-//     headers: {
-//       "X-RapidAPI-Key": "e06e57cf76msh424f6b75f530eadp1dd9a4jsn5efa8229c5f2",
-//       "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
-//     },
-//   };
+// // const allExercices = async function () {
+// //   const url = "https://exercisedb.p.rapidapi.com/exercises";
+// //   const options = {
+// //     method: "GET",
+// //     headers: {
+// //       "X-RapidAPI-Key": "e06e57cf76msh424f6b75f530eadp1dd9a4jsn5efa8229c5f2",
+// //       "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+// //     },
+// //   };
 
-//   try {
-//     const response = await fetch(url, options);
-//     const result = await response.json();
-//     const { exercises } = result;
-//     console.log();
+// //   try {
+// //     const response = await fetch(url, options);
+// //     const result = await response.json();
+// //     const { exercises } = result;
+// //     console.log();
 
-//     const [...alldata] = [...result.values()];
-//     console.log(alldata);
-//     const allEquipment = alldata.map((el) => el);
+// //     const [...alldata] = [...result.values()];
+// //     console.log(alldata);
+// //     const allEquipment = alldata.map((el) => el);
 
-//     const bodyWeight = allEquipment.filter((equip, i) =>  equip.bodyPart === 'waist' && equip.equipment === 'body weight');
+// //     const bodyWeight = allEquipment.filter((equip, i) =>  equip.bodyPart === 'waist' && equip.equipment === 'body weight');
 
-//     console.log(bodyWeight);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
+// //     console.log(bodyWeight);
+// //   } catch (error) {
+// //     console.error(error);
+// //   }
+// // };
 
-// allExercices();
+// // allExercices();
 
 function getLocation() {
   if (navigator.geolocation) {
